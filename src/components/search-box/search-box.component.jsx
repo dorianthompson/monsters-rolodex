f859382b6f618978c9ce6d0f109c423a6353f488
Dorian Thompson <thompsondorian2@gmail.com>
2022-05-15 22:17:07 -0400

    push monster-rolodex code to github

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+import './search-box.styles.css';
+
+const SearchBox = ({ className, placeholder, onSearchChange } ) => {
+  
+    return (
+        <input 
+            className={`search-box ${className}`}
+            type="search" 
+            placeholder={placeholder}
+            onChange={onSearchChange}
+        /> 
+    );
+    
+}
+
+export default SearchBox;
